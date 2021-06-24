042798eaa7977bd8ffe7e4286653306fa0baaee6
Michael Lowe <56036696+MicholasFlat@users.noreply.github.com>
2021-06-24 13:54:11 -0400

    AVL custom insert stuff
    bababoulli

--- AlgoVision/Assets/Scripts/Trees/AVLUI.cs
+++ AlgoVision/Assets/Scripts/Trees/AVLUI.cs
@@ -11,7 +11,6 @@ public class AVLUI : MonoBehaviour
     [SerializeField] GameObject spherePrefab;
     [SerializeField] GameObject canvas;
     [SerializeField] Slider speedSlider;
-    [SerializeField] GameObject inputText;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +21,6 @@ public class AVLUI : MonoBehaviour
         //a.time = speedSlider.value;
         canvas = GameObject.Find("Canvas");
         speedSlider = canvas.transform.GetChild(1).GetComponent<Slider>();
-       // inputText = canvas.transform.GetChild(13);
         switch (startSize)
         {
             case 0:
@@ -47,12 +45,12 @@ public class AVLUI : MonoBehaviour
                 {
                     //inputText.SetActive(false);
                     //a.testInserts();
-                    int[] arrayA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 16, -12, -14, -6, -7, -8 };
+                   /* int[] arrayA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 16, -12, -14, -6, -7, -8 };
                     int[] arrayB = { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 30, 32, -30, -30, -14 };
                     a.customInserts(arrayA);
                     StartCoroutine(a.readQueue());
                     a.customInserts(arrayB);
-                    StartCoroutine(a.readQueue());
+                    StartCoroutine(a.readQueue());*/
                     //convert input field value to integers, if possible
                     //send integers to insertion
                     break;
@@ -72,8 +70,23 @@ public class AVLUI : MonoBehaviour
         a.time = speedSlider.value;
     }
 
-    void insertKeys()
+    void insertKeys()// attach inputfield on the canvas to this functionStartCoroutine(a.readQueue());
     {
 
+        string inputs = "";//= canvas.transform.GetChild(13).GetComponent<TMP_Text>().text;// grab string from canvas
+
+        // clear text input field
+
+        string[] textInputs = inputs.Split(',');// split string into tokens by the commas
+        int[] keys = new int[textInputs.Length];
+
+
+        // converting string into arrays
+            // check if its a valid string
+                // no letters
+                // no 0's
+
+        // send that shit to customInsert()
+        // StartCoroutine(a.readQueue());
     }
 }
