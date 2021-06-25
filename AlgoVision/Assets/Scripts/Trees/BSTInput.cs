68a458e0b2b1e6478559e1808661c92f587c7469
CodyOliver64 <56767738+CodyOliver64@users.noreply.github.com>
2021-06-25 14:39:09 -0400

    Creating BST Scene

---
+++ AlgoVision/Assets/Scripts/Trees/BSTInput.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class BSTInput : MonoBehaviour
+{
+    [SerializeField] GameObject spherePrefab;
+    public AVL b;
+    [SerializeField] Slider speedSlider;
+    [SerializeField] GameObject inputField;
+
+    void Start()
+    {
+        speedSlider = GameObject.Find("Canvas").transform.GetChild(1).GetComponent<Slider>();
+        inputField = GameObject.Find("Canvas").transform.GetChild(13).gameObject;
+        b = new AVL(0, spherePrefab);
+    }
+    public void umsthing()
+    {
+        Debug.Log("Current TIME: " + b.time);
+        speedSlider = GameObject.Find("Canvas").transform.GetChild(1).GetComponent<Slider>();
+        b = new AVL(0, spherePrefab);
+    }
+    void Update()
+    {
+        b.time = speedSlider.value;
+        //Debug.Log("After TIME: " + b.time);
+    }
+
+    public void readInput()
+    {
+        bool yagud = true;
+        string inputs = inputField.GetComponent<TMP_InputField>().text;
+        Debug.Log(inputs);
+        // clear text input field
+
+        string[] textInputs = inputs.Split(',');// split string into tokens by the commas
+        int[] keys = new int[textInputs.Length];
+
+        for(int i = 0; i < keys.Length; i++)
+        {
+            try
+            {
+                keys[i] = Int32.Parse(textInputs[i]);
+            }
+            catch(FormatException)
+            {
+                GameObject.Find("Canvas").transform.GetChild(5).GetComponent<TMP_Text>().text = "Can not insert " + textInputs[i] + " because it is not a number";
+                Debug.Log("Cant convert " + textInputs[i] + " to int");
+                keys[i] = -1;
+                yagud = false;
+            }
+
+            if(keys[i] == 0)
+            {
+                GameObject.Find("Canvas").transform.GetChild(5).GetComponent<TMP_Text>().text = "Please remove 0 from your inserts.";
+                yagud = false;
+            }
+
+            if(keys[i] > 999 || keys[i] < -999)
+            {
+                yagud = false;
+                GameObject.Find("Canvas").transform.GetChild(5).GetComponent<TMP_Text>().text = "Please only insert or delete numbers between 1 and 999.";
+            }
+        }
+
+        if(yagud)
+        {
+            inputField.GetComponent<TMP_InputField>().text = "";
+            b.customInserts(keys);
+            StartCoroutine(b.readQueue());
+        }
+    }
+}
---
+++ AlgoVision/Assets/Scripts/Trees/BSTUI.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+public class BSTUI : MonoBehaviour
+{
+
+    [SerializeField] AVL a;
+    [SerializeField] GameObject spherePrefab;
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject cam;
+    [SerializeField] Slider speedSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = new AVL(0, spherePrefab);
+        int startSize = FindObjectOfType<TMP_Dropdown>().value;
+        //FindObjectOfType<TMP_InputField>.SetActive(false); // makes input field invisible
+        //a.time = speedSlider.value;
+        canvas = GameObject.Find("Canvas");
+        speedSlider = canvas.transform.GetChild(1).GetComponent<Slider>();
+        canvas.transform.GetChild(13).gameObject.SetActive(false);
+        switch (startSize)
+        {
+            case 0:
+                {
+                    Camera.main.orthographicSize = 14;
+                    a.insertRandom(7);
+                    StartCoroutine(a.readQueue());
+                    break;
+                }
+            case 1:
+                {
+                    Camera.main.orthographicSize = 14;
+                    a.insertRandom(15);
+                    StartCoroutine(a.readQueue());
+                    break;
+                }
+            case 2:
+                {
+
+                    Camera.main.orthographicSize = 24;
+                    a.insertRandom(31);
+                    StartCoroutine(a.readQueue());
+                    break;
+                }
+            case 3:
+                {
+                    Camera.main.orthographicSize = 14;
+                    canvas.transform.GetChild(5).GetComponent<TMP_Text>().text = "Choose Values To Insert or Delete!";
+                    canvas.transform.GetChild(13).gameObject.SetActive(true);
+                    //inputText.SetActive(false);
+                    //a.testInserts();
+                   /* int[] arrayA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 16, -12, -14, -6, -7, -8 };
+                    int[] arrayB = { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 30, 32, -30, -30, -14 };
+                    a.customInserts(arrayA);
+                    StartCoroutine(a.readQueue());
+                    a.customInserts(arrayB);
+                    StartCoroutine(a.readQueue());*/
+                    //convert input field value to integers, if possible
+                    //send integers to insertion
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Uh oh, i made a fucky wucky");
+                    break;
+                }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.time = speedSlider.value;
+    }
+
+    void insertKeys()// attach inputfield on the canvas to this functionStartCoroutine(a.readQueue());
+    {
+
+        string inputs = "";//= canvas.transform.GetChild(13).GetComponent<TMP_Text>().text;// grab string from canvas
+
+        // clear text input field
+
+        string[] textInputs = inputs.Split(',');// split string into tokens by the commas
+        int[] keys = new int[textInputs.Length];
+
+
+        // converting string into arrays
+            // check if its a valid string
+                // no letters
+                // no 0's
+
+        // send that shit to customInsert()
+        // StartCoroutine(a.readQueue());
+    }
+}
