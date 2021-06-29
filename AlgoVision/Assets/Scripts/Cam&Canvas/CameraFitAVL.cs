b6f5f62074b0a24e08e3b1863be55ffd0d18c58a
CodyOliver64 <56767738+CodyOliver64@users.noreply.github.com>
2021-06-29 16:09:39 -0400

    Updated all scenes to include Aspect Ratio Fix

---
+++ AlgoVision/Assets/Scripts/Cam&Canvas/CameraFitAVL.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CameraFitAVL : MonoBehaviour
+{
+    public bool maitainWidth = true;
+    float defaultWidth;
+    Vector3 CameraPos;
+    int startSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startSize = FindObjectOfType<TMP_Dropdown>().value;
+        
+        if (startSize != 2)
+        {
+            defaultWidth = 14.0f * 1.825866f;
+
+        }
+        else
+        {
+            defaultWidth = 24.0f * 1.825866f;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maitainWidth)
+        {
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+            //Camera.main.transform.position = new Vector3(CameraPos.x, -1 * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+            //Camera.main.transform.position = new Vector3(CameraPos.x, CameraPos.y, CameraPos.z);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Refresh()
+    {
+        startSize = FindObjectOfType<TMP_Dropdown>().value;
+
+        if (startSize != 2)
+        {
+            defaultWidth = 6.0f * 2.625866f;
+
+        }
+        else
+        {
+            defaultWidth = 8.5f * 2.625866f;
+        }
+
+    }
+}
---
+++ AlgoVision/Assets/Scripts/Cam&Canvas/CameraFitGraph.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CameraFitGraph : MonoBehaviour
+{
+    public bool maitainWidth = true;
+    float defaultWidth;
+    Vector3 CameraPos;
+    int startSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        defaultWidth = 6.0f * 2.025866f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maitainWidth)
+        {
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+            //Camera.main.transform.position = new Vector3(CameraPos.x, -1 * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+            //Camera.main.transform.position = new Vector3(CameraPos.x, CameraPos.y, CameraPos.z);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Refresh()
+    {
+
+
+        defaultWidth = 6.0f * 2.625866f;
+
+    }
+}
