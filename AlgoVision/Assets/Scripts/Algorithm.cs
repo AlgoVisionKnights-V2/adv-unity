e6534f8fbdaa3a381147b6307b12d9d630fa3d60
mweston6288 <mweston6288@knights.ucf.edu>
2021-07-10 09:43:10 -0400

    Added an initial list of enumerators to algorithm

--- AlgoVision/Assets/Scripts/Algorithm.cs
+++ AlgoVision/Assets/Scripts/Algorithm.cs
@@ -10,4 +10,34 @@ public abstract class Algorithm : MonoBehaviour
     public float time, completionPercent;
     public long stopTime, currentTime;
     public static Stopwatch timer = new System.Diagnostics.Stopwatch();
+
+    public enum Commands{
+        WAIT,
+        COLOR_ONE,
+        COLOR_TWO,
+        SWAP,
+        RAISE,
+        LOWER,
+        COLORALL,
+        UPDATE_MESSAGE,
+        UPDATE_OBJECT_TEXT,
+        RAISE_ALL,
+        LOWER_ALL,
+        COPY_MAIN,
+        COPY_AUX,
+        HIDE,
+        SHOW
+    }
+    public enum Array {
+        MAIN,
+        AUX
+    }
+    public enum Colors {
+        WHITE,
+        BLACK,
+        RED,
+        BLUE,
+        GREEN,
+        YELLOW
+    }
 }
